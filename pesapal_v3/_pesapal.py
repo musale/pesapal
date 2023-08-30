1a3e3c43d1c6667f62f0fb6cd1af7fc299437730
Musale Martin <martinmshale@gmail.com>
2023-08-30 22:33:02 +0300

    Fix build and typing errors
    Signed-off-by: Musale Martin <martinmshale@gmail.com>

--- pesapal_v3/_pesapal.py
+++ pesapal_v3/_pesapal.py
@@ -5,20 +5,12 @@ from typing import Dict, List, Optional
 
 import httpx
 
-from pesapal_v3._exceptions import (
-    PesapalAuthError,
-    PesapalIPNURLRegError,
-    PesapalSubmitOrderRequestError,
-    PesapalListIPNsError,
-)
-from pesapal_v3._types import (
-    AccessToken,
-    Environment,
-    IPNRegistration,
-    OrderRequest,
-    PesapalError,
-    OrderRequestResponse,
-)
+from pesapal_v3._exceptions import (PesapalAuthError, PesapalIPNURLRegError,
+                                    PesapalListIPNsError,
+                                    PesapalSubmitOrderRequestError)
+from pesapal_v3._types import (AccessToken, Environment, IPNRegistration,
+                               OrderRequest, OrderRequestResponse,
+                               PesapalError)
 
 
 class Pesapal:
@@ -68,9 +60,7 @@ class Pesapal:
                 consumer_key=self._consumer_key, consumer_secret=self._consumer_secret
             )
 
-    def _authenticate(
-        self, *, consumer_key: str, consumer_secret: str
-    ) -> Optional[AccessToken]:
+    def _authenticate(self, *, consumer_key: str, consumer_secret: str) -> AccessToken:
         with httpx.Client(base_url=self._base_url) as client:
             data = {"consumer_key": consumer_key, "consumer_secret": consumer_secret}
             client_resp = client.post(
@@ -84,8 +74,7 @@ class Pesapal:
                 )
             token = response.get("token", "")
             self._headers.update({"Authorization": f"Bearer {token}"})
-            access_token: Optional[AccessToken] = AccessToken(**response)
-        return access_token
+        return AccessToken(**response)
 
     def update_headers(self, *, headers: Dict[str, str]) -> None:
         """Updates the header values."""
@@ -141,16 +130,17 @@ class Pesapal:
                 headers=self._headers,
             )
             response: List[IPNRegistration] = client_resp.json()
-        error = response.get("error", None)
-        status = response.get("status", "500")
-        if error:
-            error_msg = PesapalError(**error)
-            raise PesapalListIPNsError(error=error_msg, status=status)
+        if isinstance(response, dict):
+            error = response.get("error", None)
+            status = response.get("status", "500")
+            if error:
+                error_msg = PesapalError(**error)
+                raise PesapalListIPNsError(error=error_msg, status=status)
         return response
 
     def submit_order_request(
         self, *, order_request: OrderRequest
-    ) -> OrderRequestResponse:
+    ) -> Optional[OrderRequestResponse]:
         """Process the request to create a payment."""
         if not order_request:
             raise ValueError("order_request cannot be empty")
@@ -168,4 +158,4 @@ class Pesapal:
         if error:
             error_msg = PesapalError(**error)
             raise PesapalSubmitOrderRequestError(error=error_msg, status=status)
-        return response
+        return OrderRequestResponse(**response)
